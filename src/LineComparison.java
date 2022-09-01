1489867d9c05aef21b7aff8d4529d623fa8aac47
succhi111 <suchendramishra396@gmail.com>
2022-09-01 16:14:56 +0530

    UC1_LENGTH OF LINE

---
+++ src/LineComparison.java
@@ -0,0 +1,15 @@
+public class LineComparison
+{
+    public static void main(String args[])
+    {
+        System.out.println("Welcome To Line Comparision Computation Program on Master Branch");
+
+        int x1=10,y1=20,x2=30,y2=40;
+
+
+        double LengthofLine =Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+
+        System.out.println("Length of Line : "+ LengthofLine);
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome To Line Comparision Computation Program on Master Branch");
     }
 }
