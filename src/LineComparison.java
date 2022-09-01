28c7c3e7c4acd74b4765153ba91bc4b51ff25808
succhi111 <suchendramishra396@gmail.com>
2022-09-01 16:29:58 +0530

    UC2_COMPARE OF LINE

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -5,11 +5,25 @@ public class LineComparison
         System.out.println("Welcome To Line Comparision Computation Program on Master Branch");
 
         int x1=10,y1=20,x2=30,y2=40;
+        int x3=10,y3=20,x4=30,y4=40;
 
 
-        double LengthofLine =Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+        double LengthofLine1 =Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
 
-        System.out.println("Length of Line : "+ LengthofLine);
+        System.out.println("Length of First Line : "+ LengthofLine1);
+
+        double LengthofLine2 =Math.sqrt(Math.pow(x4-x3,2) + Math.pow(y4-y3,2));
+
+        System.out.println("Length of Second Line : "+ LengthofLine2);
+
+        if(LengthofLine1 == LengthofLine2)
+        {
+            System.out.println("Two lines are Equal");
+        }
+        else
+        {
+            System.out.println("Two lines are Not Equal");
+        }
+
     }
 }
-
